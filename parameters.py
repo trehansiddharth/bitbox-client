65261effd98cf8865a52fe5e0323872152928ff0
Sid Trehan <trehansiddharth@gmail.com>
2022-12-28 13:28:37 -0800

    Set certain parameters to be configurable by environment variables

--- parameters.py
+++ parameters.py
@@ -1,10 +1,17 @@
 import os
 from errors import *
 
-BITBOX_HOST = "localhost:8000"
-BITBOX_CONFIG_FOLDER = os.path.join(os.getcwd(), ".bitbox")
-BITBOX_CLIENT_NAME_LENGTH = 10
+#
+# Configurable parameters
+#
+
+BITBOX_HOST = os.environ.get("BITBOX_HOST") or "sidtrehan.me:8000"
+BITBOX_CONFIG_FOLDER = os.environ.get("BITBOX_CONFIG_FOLDER") or os.path.join(os.path.expanduser("~"), ".bitbox")
+
+#
+# Other parameters
+#
+
 BITBOX_STATUS_OK = 200
-BITBOX_FILE_KEY_LENGTH = 128
 BITBOX_USERNAME_REGEX = r"^[a-z0-9]+$"
 BITBOX_FILENAME_REGEX = r"^@[a-z0-9]+\/[^\/\r\n ]+$"
